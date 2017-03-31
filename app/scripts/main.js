cc1636dff8dfef29915e0454924cf8e6c703e8b5
Alessandro Capra <alessandro.cpr@gmail.com>
2017-04-01 00:30:34 +0300

    Fixed visual stuff + smooth transition when clicking on menu items

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -24,3 +24,18 @@
     slides[slideIndex-1].style.display = 'flex';
     dots[slideIndex-1].className += ' active';
   }
+
+  $(function() {
+    $('a[href*="#"]:not([href="#"])').click(function() {
+      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+        if (target.length) {
+          $('html, body').animate({
+            scrollTop: target.offset().top
+          }, 1000);
+          return false;
+        }
+      }
+    });
+  });
